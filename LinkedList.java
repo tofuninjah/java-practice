b24a02c8e3f0403ff93467fc0cf2d03c14e7e54e
Kang, Chung <ckang@nrgenergy.com>
2018-11-15 14:36:06 -0600

    Start LinkedLink Exercises

---
+++ LinkedList.java
@@ -0,0 +1,129 @@
+import java.util.*;
+
+/**
+ * Java Collection: 22 ArrayList Exercises
+ */
+class LinkedListDemo {
+    public static void main(String args[]) {
+        List<String> aLinkedList = new LinkedList<>();
+        aLinkedList.add("default-a");
+        aLinkedList.add("default-b");
+        aLinkedList.add("default-c");
+        aLinkedList.add("default-d");
+        aLinkedList.add("default-e");
+
+        /**
+         * 1. Append the specified element to the end of a linked list
+         */
+        String element = "new-a";
+        System.out.println(aLinkedList);
+        aLinkedList.add(element);
+        System.out.println(aLinkedList);
+
+        /**
+         * 2. Iterate through all elements in a linked list
+         */
+        for (String el : aLinkedList) {
+            System.out.println(el);
+        }
+
+        /**
+         * 3. Iterate through all elements in a linked list starting at the specified
+         * position.
+         */
+
+        /**
+         * 4. Iterate a linked list in reverse order.
+         */
+
+        /**
+         * 5. Insert the specified element at the specified position in the linked list.
+         */
+
+        /**
+         * 6. Insert elements into the linked list at the first and last position.
+         */
+
+        /**
+         * 7. Insert the specified element at the front of a linked list.
+         */
+
+        /**
+         * 8. Insert the specified element at the end of a linked list.
+         */
+
+        /**
+         * 9. Insert some elements at the specified position into a linked list.
+         */
+
+        /**
+         * 10. get the first and last occurrence of the specified elements in a linked
+         * list.
+         */
+
+        /**
+         * 11. display the elements and their positions in a linked list.
+         */
+
+        /**
+         * 12. remove a specified element from a linked list.
+         */
+
+        /**
+         * 13. remove first and last element from a linked list.
+         */
+
+        /**
+         * 14. remove all the elements from a linked list.
+         */
+
+        /**
+         * 15. Write a Java program of swap two elements in an linked list.
+         */
+
+        /**
+         * 16. shuffle the elements in a linked list.
+         */
+
+        /**
+         * 17. join two linked lists.
+         */
+
+        /**
+         * 18. clone an linked list to another linked list.
+         */
+
+        /**
+         * 19. remove and return the first element of a linked list.
+         */
+
+        /**
+         * 20. retrieve but does not remove, the first element of a linked list.
+         */
+
+        /**
+         * 21. retrieve but does not remove, the last element of a linked list.
+         */
+
+        /**
+         * 22. check if a particular element exists in a linked list.
+         */
+
+        /**
+         * 23. convert a linked list to array list.
+         */
+
+        /**
+         * 24. compare two linked lists.
+         */
+
+        /**
+         * 25. test an linked list is empty or not.
+         */
+
+        /**
+         * 26. replace an element in a linked list.
+         */
+
+    }
+}
